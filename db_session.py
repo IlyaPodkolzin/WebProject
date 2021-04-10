bfb6d4491807603b08209ac77d65eced4ca7a8fd
vadimkocetok <koshetok.com@mail.ru>
2021-04-10 15:07:16 +0300

    add db without all models

---
+++ db_session.py
@@ -0,0 +1,25 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+import sqlalchemy.ext.declarative as dec
+import db_models
+
+ORMBase = dec.declarative_base()
+__factory = None
+global_session = None
+
+
+def create_session():
+    global __factory
+    return __factory()
+
+
+def global_init(db_file):
+    global __factory, global_session
+    if __factory:
+        return
+    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    engine = sa.create_engine(conn_str, echo=False)
+    __factory = orm.sessionmaker(bind=engine)
+    ORMBase.metadata.create_all(engine)
+    global_session = create_session()
