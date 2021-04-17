e875b6127fb371168ef9e12e9609ade3cd485103
IlyaPodkolzin <pdk.04@mail.ru>
2021-04-17 20:31:09 +0300

    Added requirements.txt, main.py connected with Heroku, added templates, changed Procfile, base.html moved to templates

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Let's start!")
+import os
+from flask import Flask, render_template
+import forms
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'check_check_key'
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
